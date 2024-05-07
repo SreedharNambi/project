ee974a6bcc3eae3a5045a401d0b1c35cdafa4ba6
sreedhar <sreedharan1997@gmail.com>
2024-05-07 15:53:37 +0530

    added db schema to readme

--- src/app.module.ts
+++ src/app.module.ts
@@ -7,10 +7,15 @@ import { RewardService } from './reward/reward.service';
 import { RewardController } from './reward/reward.controller';
 import { P5Controller } from './p5/p5.controller';
 import { P5Service } from './p5/p5.service';
+import { PostgresService } from './database/postgres.service';
+import postgresConfig from './database/postgres.config';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [],
+  imports: [ConfigModule.forRoot({
+    load: [postgresConfig ]
+    })],
   controllers: [AppController,  UserController, RewardController, P5Controller],
-  providers: [AppService, UserService, RewardService, P5Service],
+  providers: [AppService, UserService, RewardService, P5Service, PostgresService],
 })
 export class AppModule {}
